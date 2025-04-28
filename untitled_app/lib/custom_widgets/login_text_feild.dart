136dc70a62432b34288ffd893408e8c881a35a66
Christian Knab <christiantknab@gmail.com>
2025-04-28 01:03:27 -0700

    removed login text field controller

--- untitled_app/lib/custom_widgets/login_text_feild.dart
+++ untitled_app/lib/custom_widgets/login_text_feild.dart
@@ -1,8 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:provider/provider.dart' as prov;
-import 'controllers/login_text_feild_controller.dart';
 import '../utilities/constants.dart' as c;
+
+class CustomInputField extends StatefulWidget {
+  const CustomInputField({super.key});
+
+  @override
+  State<CustomInputField> createState() => _CustomInputField();
+}
+
+class _CustomInputField extends State<CustomInputField> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
 
 class CustomInputFeild extends StatelessWidget {
   final int? maxLen;
@@ -46,86 +58,72 @@ class CustomInputFeild extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     double feildWidth;
+    bool hidden = password;
 
     if (width == null) {
       feildWidth = c.widthGetter(context) * 0.9;
     } else {
       feildWidth = width!;
     }
-    return prov.ChangeNotifierProvider(
-        create: (context) => LoginFieldController(password: password),
-        builder: (context, child) {
-          return Container(
-            alignment: Alignment.bottomCenter,
-            padding: padding
-                ? const EdgeInsets.only(top: 10, bottom: 10)
-                : const EdgeInsets.only(),
-            width: feildWidth,
-            height: height,
-            child: TextFormField(
-              maxLength: maxLen,
-              cursorColor: Theme.of(context).colorScheme.onSurface,
-              obscureText:
-                  prov.Provider.of<LoginFieldController>(context, listen: true)
-                      .hidden,
+    return Container(
+      alignment: Alignment.bottomCenter,
+      padding: padding
+          ? const EdgeInsets.only(top: 10, bottom: 10)
+          : const EdgeInsets.only(),
+      width: feildWidth,
+      height: height,
+      child: TextFormField(
+        maxLength: maxLen,
+        cursorColor: Theme.of(context).colorScheme.onSurface,
+        obscureText: hidden,
 
-              enabled: enabled,
-              inputFormatters: [
-                FilteringTextInputFormatter.allow(RegExp(filter)),
-              ],
-              textInputAction: textInputAction,
-              autovalidateMode: validator,
-              validator: validatorFunction,
-              controller: controller,
-              focusNode: focus,
-              onChanged: onChanged,
-              onEditingComplete: onEditingComplete,
-              //autofocus: true,
-              keyboardType: inputType,
-              style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.normal,
-                  color: Theme.of(context).colorScheme.onSurface),
-              decoration: InputDecoration(
-                counterText: showCounter ? null : '',
-                labelText: label,
-                labelStyle: TextStyle(
-                  fontSize: 18,
-                  letterSpacing: 1,
-                  fontWeight: FontWeight.normal,
-                  color: Theme.of(context).colorScheme.onSurface,
-                ),
-                fillColor: Theme.of(context)
-                    .colorScheme
-                    .onSurface
-                    .withValues(alpha: 0.2),
-                filled: true,
-                enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(
-                      color: Theme.of(context).colorScheme.outlineVariant),
-                  borderRadius: BorderRadius.circular(10.0),
-                ),
-                focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10.0),
-                    borderSide: BorderSide(
-                        color: Theme.of(context).colorScheme.onSurface)),
-                suffixIcon: password
-                    ? IconButton(
-                        icon: Icon(prov.Provider.of<LoginFieldController>(
-                                    context,
-                                    listen: true)
-                                .hidden
-                            ? Icons.visibility_off
-                            : Icons.visibility),
-                        onPressed: () => prov.Provider.of<LoginFieldController>(
-                                context,
-                                listen: false)
-                            .bottonPressed(),
-                      )
-                    : null,
-              ),
-            ),
-          );
-        });
+        enabled: enabled,
+        inputFormatters: [
+          FilteringTextInputFormatter.allow(RegExp(filter)),
+        ],
+        textInputAction: textInputAction,
+        autovalidateMode: validator,
+        validator: validatorFunction,
+        controller: controller,
+        focusNode: focus,
+        onChanged: onChanged,
+        onEditingComplete: onEditingComplete,
+        //autofocus: true,
+        keyboardType: inputType,
+        style: TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.normal,
+            color: Theme.of(context).colorScheme.onSurface),
+        decoration: InputDecoration(
+          counterText: showCounter ? null : '',
+          labelText: label,
+          labelStyle: TextStyle(
+            fontSize: 18,
+            letterSpacing: 1,
+            fontWeight: FontWeight.normal,
+            color: Theme.of(context).colorScheme.onSurface,
+          ),
+          fillColor:
+              Theme.of(context).colorScheme.onSurface.withValues(alpha: 0.2),
+          filled: true,
+          enabledBorder: OutlineInputBorder(
+            borderSide:
+                BorderSide(color: Theme.of(context).colorScheme.outlineVariant),
+            borderRadius: BorderRadius.circular(10.0),
+          ),
+          focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(10.0),
+              borderSide:
+                  BorderSide(color: Theme.of(context).colorScheme.onSurface)),
+          suffixIcon: password
+              ? IconButton(
+                  icon: Icon(hidden ? Icons.visibility_off : Icons.visibility),
+                  onPressed: () {
+                    hidden = !hidden;
+                  })
+              : null,
+        ),
+      ),
+    );
   }
 }
