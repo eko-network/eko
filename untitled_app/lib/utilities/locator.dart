3d227048456f9434269002d154185c286a87decc
Eliah Reeves <ereeclimb@gmail.com>
2025-06-07 15:19:41 -0700

    update enforcement

--- untitled_app/lib/utilities/locator.dart
+++ untitled_app/lib/utilities/locator.dart
@@ -1,11 +1,9 @@
 import 'package:get_it/get_it.dart';
 import 'package:untitled_app/models/presence_manager.dart';
 import '../models/current_user.dart';
-import '../models/version_control.dart';
 
 final locator = GetIt.instance;
 void setupLocator() {
   locator.registerLazySingleton<CurrentUser>(() => CurrentUser());
   locator.registerSingleton<PresenceManager>(PresenceManager());
-  locator.registerSingleton<Version>(Version());
 }
